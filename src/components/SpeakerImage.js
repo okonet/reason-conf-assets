c0e60ce43315b883b743f1a8c4e7692cedbd0563
Andrey Okonetchnikov <andrey@okonet.ru>
2019-03-16 10:26:26 +0100

    Match SpeakerImage to Figma design

--- src/components/SpeakerImage.js
+++ src/components/SpeakerImage.js
@@ -5,13 +5,13 @@ const Picture = styled('picture')`
   position: relative;
   display: block;
   width: 100%;
-  filter: drop-shadow(20px 20px 0px rgba(0,0,0,0.25));
+  filter: drop-shadow(15px 15px 0px rgba(0,0,0,0.25));
   
   img {
     display: block;
     margin: 0;
     padding: 0;
-    clip-path: polygon(0 15px, 100% 0, 100% calc(100% - 15px), 0 100%);
+    clip-path: polygon(0 10px, 100% 0, 100% calc(100% - 10px), 0 100%);
     filter: grayscale(100%);
   }
 `
