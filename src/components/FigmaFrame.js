349d1b428663f626df863acff3a3e0752d9b3fef
Andrey Okonetchnikov <andrey@okonet.ru>
2019-03-16 13:48:09 +0100

    Enable sync for FigmaFrame

--- src/components/FigmaFrame.js
+++ src/components/FigmaFrame.js
@@ -2,6 +2,7 @@ import React from "react"
 import styled from "@emotion/styled"
 import { Query } from "urql"
 import gql from "graphql-tag"
+import { LastModifiedContext } from "./layout"
 
 const NodeWrapper = styled("div")`
   position: relative;
@@ -55,67 +56,68 @@ const getImagesOfNode = gql`
 
 export default function FigmaFrame({ fileId, pageName, nodeName, children }) {
   return (
-    <Query
-      query={getFigmaNode}
-      variables={{
-        fileId,
-        pageName,
-        nodeName,
-      }}
-    >
-      {({ fetching, data, error }) => {
-        console.log(data)
-        if (fetching) {
-          return "Loading..."
-        } else if (error) {
-          return "Oh no!"
-        } else if (!data) {
-          return null
-        }
+    <LastModifiedContext.Consumer>
+      {lastModified => (
+        <Query
+          query={getFigmaNode}
+          variables={{
+            fileId,
+            pageName,
+            nodeName,
+            lastModified
+          }}
+        >
+          {({ fetching, data, error }) => {
+            if (error) {
+              console.error(error)
+              return "Oh no!"
+            } else if (!data) {
+              return null
+            }
 
-        const frame = data.file.pages[0].frames[0]
-        const { size } = frame
-        const { id } = frame.children[0]
+            const frame = data.file.pages[0].frames[0]
+            const { size } = frame
+            const { id } = frame.children[0]
 
-        return (
-          <NodeWrapper
-            css={{
-              ...size,
-            }}
-          >
-            <Query
-              query={getImagesOfNode}
-              variables={{
-                fileId,
-                nodeIds: id,
-              }}
-            >
-              {({ fetching, data, error }) => {
-                console.log(data)
-                if (fetching) {
-                  return "Loading image..."
-                } else if (error) {
-                  console.error(error)
-                  return "Image loading failed!"
-                } else if (!data) {
-                  return null
-                }
+            return (
+              <NodeWrapper
+                css={{
+                  ...size,
+                }}
+              >
+                <Query
+                  query={getImagesOfNode}
+                  variables={{
+                    fileId,
+                    nodeIds: id,
+                    lastModified
+                  }}
+                >
+                  {({ fetching, data, error }) => {
+                    if (error) {
+                      console.error(error)
+                      return "Oh no!"
+                    } else if (!data) {
+                      return null
+                    }
 
-                return (
-                  <NodeWrapper
-                    css={{
-                      ...size,
-                      background: `url(${data.image.images[0]})`,
-                    }}
-                  >
-                    {children}
-                  </NodeWrapper>
-                )
-              }}
-            </Query>
-          </NodeWrapper>
-        )
-      }}
-    </Query>
+                    return (
+                      <NodeWrapper
+                        css={{
+                          ...size,
+                          background: `url(${data.image.images[0]})`,
+                        }}
+                      >
+                        {children}
+                      </NodeWrapper>
+                    )
+                  }}
+                </Query>
+              </NodeWrapper>
+            )
+          }}
+        </Query>
+      )}
+    </LastModifiedContext.Consumer>
   )
 }
