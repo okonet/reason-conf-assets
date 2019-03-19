e1c9e2d1c7da44c659c44af45b8405be2ec006b2
Andrey Okonetchnikov <andrey@okonet.ru>
2019-03-19 11:09:27 +0100

    Update repo metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,46 +1,41 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: "Reason Conf Print Asset Generator",
+    description: "Print assets generator for ReasonConf",
+    author: "@okonet",
   },
   plugins: [
     "gatsby-plugin-styled-components",
-    `gatsby-plugin-typescript`,
-    `gatsby-plugin-react-helmet`,
+    "gatsby-plugin-typescript",
+    "gatsby-plugin-react-helmet",
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: `images`,
+        name: "images",
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: "gatsby-plugin-manifest",
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        name: "reason-conf-assets",
+        short_name: "assets",
+        start_url: "/",
+        background_color: "#395672",
+        theme_color: "#DD4B39",
+        display: "minimal-ui",
+        icon: "src/images/gatsby-icon.png", // This path is relative to the root of the site.
       },
     },
     {
       resolve: "gatsby-source-graphql",
       options: {
         typeName: "DATA",
-
         fieldName: "data",
-
         url: "http://localhost:3000/graphql",
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // 'gatsby-plugin-offline',
   ],
 }
