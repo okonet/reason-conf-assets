d57fcf7d541ccb7e9cf595974246f72f8f8ea0ef
Andrey Okonetchnikov <andrey@okonet.ru>
2019-03-28 21:21:59 +0100

    Do not use StaticQuery and use static data for badge design

--- src/pages/badge.js
+++ src/pages/badge.js
@@ -1,43 +1,29 @@
 import React from "react"
-import { graphql, Link, StaticQuery } from "gatsby"
+import { Link } from "gatsby"
+import { File } from "react-figma-primitives"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Badge from "../components/Badge"
 
 const SecondPage = () => (
-    <Layout>
-      <SEO title="Badge" />
-      <h1>Badge</h1>
-      <Link to="/">Go back to the homepage</Link>
-      <StaticQuery
-        query={graphql`
-          {
-            data {
-              contact(
-                contactName: "Andrey Okonetchnikov"
-                conferenceId: "react-finland-2019"
-              ) {
-                name
-                about
-                company
-                image {
-                  url
-                }
-                talks {
-                  title
-                }
-                type
-              }
-            }
-          }
-        `}
-        render={data => (
-            <Badge
-              contact={data.data.contact}
-            />
-        )}
-      />
-    </Layout>
+  <Layout>
+    <SEO title="Badge" />
+    <h1>Badge</h1>
+    <Link to="/">Go back to the homepage</Link>
+    <File fileId="HRK5IaLG00uj7oEV2XOg45" pageName="Badge">
+      {() => (
+        <Badge
+          contact={{
+            firstName: "Andrey",
+            lastName: "Okonetchnikov",
+            company: null,
+            type: "Speaker",
+            twitter: "@okonetchnikov",
+          }}
+        />
+      )}
+    </File>
+  </Layout>
 )
 
 export default SecondPage
