62e3a3d9cbed8c5dc9b05872d2db3d46b4e167ca
Andrey Okonetchnikov <andrey@okonet.ru>
2019-03-28 21:20:16 +0100

    fix: Override some styles to allow text to flow and not overlap

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,13 +1,38 @@
 import React from "react"
 import { Frame, Text } from "react-figma-primitives"
+import Stack from "stack-styled"
 
 function Badge({ contact }) {
-  const { firstName, lastName, company } = contact
+  const { firstName, lastName, company, twitter, type } = contact
+  const stylesOverride = {
+    position: "relative",
+    top: "auto",
+    left: "auto",
+    width: "auto",
+    height: "auto",
+  }
   return (
-    <Frame nodeName="Export">
-      <Text nodeName="firstName">{firstName}</Text>
-      <Text nodeName="lastName">{lastName}</Text>
-      <Text nodeName="companyName">{company}</Text>
+    <Frame frameName={type} nodeName="Export">
+      <Stack
+        css={{
+          position: "relative",
+          top: 230,
+          margin: 30,
+        }}
+      >
+        <Text nodeName="firstName" styles={stylesOverride}>
+          {firstName}
+        </Text>
+        <Text nodeName="lastName" styles={stylesOverride}>
+          {lastName}
+        </Text>
+        <Text
+          nodeName="companyName"
+          styles={{ ...stylesOverride, marginTop: "1em" }}
+        >
+          {twitter || company}
+        </Text>
+      </Stack>
     </Frame>
   )
 }
